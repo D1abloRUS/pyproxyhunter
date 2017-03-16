bbabef8c8838f415d18e641395d2971278fcc94f
Sergey Monakhov <monakhov@ordbuy.com>
2017-03-16 16:33:41 +0300

    fix tab

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ if [ $# -eq 0 ]; then
   sed -i "s|threads=500|threads=${TREADS}|g" pyproxyhunter.py
   sed -i "s|pages=1|pages=${PAGES}|g" pyproxyhunter.py
   python pyproxyhunter.py
+  sed -i 's/\t//' output.txt
   go-proxycheck -treds=${JOBS} -url=${SITE} -in=output.txt -out=${DIR}/goodlist.txt
 else
   exec "$@"
